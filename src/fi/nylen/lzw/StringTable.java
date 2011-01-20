40379e47cf0c61c0f2e582457df07d6f618461d7
Mikko Nylén <mikko.nylen@gmail.com>
2011-01-20 20:46:29 +0200

    Added support for adding strings to the table and retrieving the code value of added strings

--- src/fi/nylen/lzw/StringTable.java
+++ src/fi/nylen/lzw/StringTable.java
@@ -1,10 +1,38 @@
 package fi.nylen.lzw;
 
+import java.util.HashMap;
+
 public class StringTable {
     public static final int STOP_CODE = 256;
     public static final int CLEAR_CODE = 257;
     private int currentCode = -1;
-    
+    private HashMap<StringEntry, Integer> entries = new HashMap<StringEntry, Integer>();
+
+    private class StringEntry {
+        private int prefixCode;
+        private int appendCharacter;
+
+        StringEntry(int prefixCode, int appendCharacter) {
+            this.prefixCode = prefixCode;
+            this.appendCharacter = appendCharacter;
+        }
+
+        @Override
+        public int hashCode() {
+            return prefixCode * appendCharacter;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof StringEntry)) {
+                return false;
+            }
+            
+            StringEntry other = (StringEntry)obj;
+            return other.prefixCode == prefixCode && other.appendCharacter == appendCharacter;
+        }
+    }
+
     /**
      * @return current code - initially <code>-1</code>
      */
@@ -31,6 +59,24 @@ public class StringTable {
             currentCode = CLEAR_CODE+1;
         } else {
             currentCode++;
+        }
+    }
+
+    /**
+     *
+     */
+    public void add(int prefixCode, byte appendCharacter) {
+        entries.put(new StringEntry(prefixCode, appendCharacter), nextCode());
+        increaseCurrentCode();
+    }
+
+    public int codeValue(int prefixCode, byte appendCharacter) {
+        StringEntry entry = new StringEntry(prefixCode, appendCharacter);
+
+        if (entries.containsKey(entry)) {
+            return entries.get(entry);
+        } else {
+            return -1;
         }
     }
 }
--- test/fi/nylen/lzw/StringTableTest.java
+++ test/fi/nylen/lzw/StringTableTest.java
@@ -7,6 +7,10 @@ import static org.junit.Assert.*;
 public class StringTableTest {
     private StringTable table;
 
+    public int ord(char c) {
+        return (int)c;
+    }
+
     @Before
     public void setUp() {
         table = new StringTable();
@@ -34,5 +38,30 @@ public class StringTableTest {
     public void increaseCurrentCodeShouldIncreaseNextCode() {
         table.increaseCurrentCode();
         assertEquals(table.currentCode()+1, table.nextCode());
+    }
+
+    @Test
+    public void addingStringShouldIncreaseCurrentCode() {
+        int nextCode = table.nextCode();
+        table.add(ord('L'), (byte)'Z');
+
+        assertEquals(table.currentCode(), nextCode);
+    }
+
+    @Test
+    public void addedStringsShouldHaveCodeValue() {
+        int codeValueForLZ = table.nextCode();
+        table.add(ord('L'), (byte)'Z');
+
+        int codeValueForHI = table.nextCode();
+        table.add(ord('H'), (byte)'I');
+
+        assertEquals(codeValueForLZ, table.codeValue(ord('L'), (byte)'Z'));
+        assertEquals(codeValueForHI, table.codeValue(ord('H'), (byte)'I'));
+    }
+
+    @Test
+    public void stringsThatHaventBeenAddedShouldHaveCodeValueMinusOne() {
+        assertEquals(-1, table.codeValue(ord('N'), (byte)'O'));
     }
 }
