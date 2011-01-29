88827eb88a96f213eefbaf56ca2b4d288daa33d1
Mikko Nylén <mikko.nylen@gmail.com>
2011-01-29 19:11:38 +0200

    Added outline for CodeWriter class

---
+++ src/fi/nylen/lzw/CodeWriter.java
@@ -0,0 +1,34 @@
+package fi.nylen.lzw;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Writes codes to <code>OutputStream</code> using a specified
+ * code width (in bits).
+ */
+public class CodeWriter {
+    private int codeWidth;
+    private OutputStream out;
+
+    /**
+     * Initializes a code writer for writing to the output stream with the
+     * initial code width.
+     *
+     * @param out stream to write to
+     * @param initialCodeWidth initial code width
+     */
+    public CodeWriter(OutputStream out, int initialCodeWidth) {
+        codeWidth = initialCodeWidth;
+        this.out  = out;
+    }
+
+    /**
+     * Writes code.
+     * @param code the code
+     * @throws IOException if anything goes wrong
+     */
+    public void write(int code) throws IOException {
+
+    }
+}
---
+++ test/fi/nylen/lzw/CodeWriterTest.java
@@ -0,0 +1,23 @@
+package fi.nylen.lzw;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.OutputStream;
+
+import static org.junit.Assert.*;
+
+public class CodeWriterTest {
+    private OutputStream out;
+    private CodeWriter writer;
+
+    @Before
+    public void setUp() {
+        
+    }
+
+    @Test
+    public void testWritingCodes() {
+        
+    }
+}
