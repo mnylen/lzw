c4772d62ec0a6d54d4f91d10883a605bcce5e4c5
Mikko Nylén <mikko.nylen@gmail.com>
2011-02-17 18:03:03 +0200

    Added class for reading codes

---
+++ src/fi/nylen/lzw/CodeReader.java
@@ -0,0 +1,42 @@
+package fi.nylen.lzw;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CodeReader {
+    private int inputBitBuffer = 0;
+    private int inputBitCount = 0;
+    private int codeWidth;
+    private InputStream in;
+    private int lastCode;
+
+    public CodeReader(InputStream in, int codeWidth) {
+        this.codeWidth = codeWidth;
+        this.in        = in;
+    }
+
+    public boolean hasNext() throws IOException {
+        return lastCode != StringTable.STOP_CODE;
+    }
+
+    public int read() throws IOException {
+        if (!(hasNext()))
+            return StringTable.STOP_CODE;
+
+        while (inputBitCount < codeWidth) {
+            int b = in.read();
+            if (b == -1)
+                return -1;
+
+            inputBitBuffer |= b << (24 - inputBitCount);
+            inputBitCount  += 8;
+        }
+
+        int returnValue = Math.abs(inputBitBuffer >>> (32 - codeWidth));
+        inputBitBuffer = inputBitBuffer << codeWidth;
+        inputBitCount -= codeWidth;
+
+        lastCode = returnValue;
+        return returnValue;
+    }
+}
